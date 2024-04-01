0682f1dfec79b47315fd66f036c6c928b280dd5a
masx200 <34191203+masx200@users.noreply.github.com>
2024-04-01 20:11:26 +0800

    Update CloudflareMiddleWare.ts

--- src/CloudflareMiddleWare.ts
+++ src/CloudflareMiddleWare.ts
@@ -16,7 +16,8 @@ export async function Strict_Transport_Security<Env>(
   headers.append("Strict-Transport-Security", "max-age=31536000");
   // console.log(ctx.response.body);
   // 必须把响应的主体转换为Uint8Array才行
-  const body = response.body && (await bodyToBuffer(response.body));
+  /* 不能全部读取,防止内存溢出,遇到大型文件,需要流式处理 */
+  const body = response.body; //&& (await bodyToBuffer(response.body));
   // headers.delete("content-length");
   const res = new Response(body, {
     status: response.status,
